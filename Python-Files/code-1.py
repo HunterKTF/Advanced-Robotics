c3149749ad6fedfb0efa3f6c2ee4ca9456f0cf3c
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2022-07-25 16:55:24 -0500

    added code 1 example

---
+++ Python-Files/code-1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+""" code-1.py
+
+	This script adds up two numbers entered by users
+"""
+
+# Import standard libraries
+import sys
+
+# Retrieve user-entered data
+number_1 = float(sys.argv[1])
+number_2 = float(sys.argv[2])
+
+# Print on the command line the addition of these two numbers
+print(number_1 + number_2)
