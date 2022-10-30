ff43480652b2a0a523b96537d48afb16b8471597
Jorge Ricardo Hernández Sabino <jhernandez@apsjoel.onmicrosoft.com>
2022-10-30 14:20:09 -0600

    added mods for car png

--- Ackermann-Sim/main.py
+++ Ackermann-Sim/main.py
@@ -20,16 +20,16 @@ args = parser.parse_args()
 print(args)
 
 # Initialize main variables
-car.tick  = 120  # Ticks to 60 fps
-car.step  = 6    # Max degree of turning (3 left and 3 right)
-car.dT    = 0.1  # Sampling time of 0.1
-car.speed = 10   # Vehicle speed in m/s
-car.l_b   = 1.2  # Distance from vehicle's center of mass to rear wheel axle
-car.l_f   = 1.4  # Distance from vehicle's center of mass to front wheel axle
-car.x0    = car.size[0] / 2 # Starts in the middle of the x axis
-car.y0    = car.size[1] / 2 # Starts in the middle of the y axis
-car.phi   = 0
-car.df    = 0
+car.tick = 120  # Ticks to 60 fps
+car.step = 6  # Max degree of turning (3 left and 3 right)
+car.dT = 0.1  # Sampling time of 0.1
+car.speed = 10  # Vehicle speed in m/s
+car.l_b = 1.2  # Distance from vehicle's center of mass to rear wheel axle
+car.l_f = 1.4  # Distance from vehicle's center of mass to front wheel axle
+car.x0 = car.size[0] / 2  # Starts in the middle of the x axis
+car.y0 = car.size[1] / 2  # Starts in the middle of the y axis
+car.phi = 0
+car.df = 0
 
 # Initialize simulation parameters
 car.init_sim_params()
@@ -50,28 +50,27 @@ car.update()
 while True:
     # Set background color to black
     car.screen.fill(BLACK)
-
+    
     # Draw tracing line
     car.tracing()
-
+    
     # Renders parameters textbox
     car.param_textbox()
-
+    
     # Renders coordinates on top of main object
     car.render_coord()
-
+    
     # Draw main object in shape of triangle
     car.draw_object()
-
+    
     # Check for pygame events
     car.check_end_event()
-
+    
     # Move main object
     car.move_object()
-
+    
     # Update display
     car.update()
-
+    
     # Setting clock tick
     car.set_clock_tick()
-
