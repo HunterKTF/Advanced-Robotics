05e4aa0504459e0179d44137539c0473ae8a12c6
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2022-11-15 17:00:25 -0600

    added functions

--- Lane-Detection/main.py
+++ Lane-Detection/main.py
@@ -9,73 +9,84 @@ from sdclibrary import Sdclib
 
 
 # Pipeline to read
-def pipeline():
+def pipeline(sdc_img):
     # 1. Read image with path provided
-    sdc_lanes.read_image()
+    sdc_img.read_image()
     
     # 2. Convert input image to greyscale
-    sdc_lanes.greyscale()
+    sdc_img.greyscale()
     
     # 3. Apply Gaussian smoothing
-    sdc_lanes.kernel_size = (9, 9)
-    sdc_lanes.gaussian_blur()
+    sdc_img.kernel_size = (9, 9)
+    sdc_img.gaussian_blur()
     
     # 4. Apply Canny Edge detection
-    sdc_lanes.low_threshold = 70
-    sdc_lanes.high_threshold = 100
-    sdc_lanes.canny_edge_det()
+    sdc_img.low_threshold = 70
+    sdc_img.high_threshold = 100
+    sdc_img.canny_edge_det()
     
     # 5. Define Region of Interest
     # Define points for vertices
     p1, p2, p3, p4, p5, p6, p7, p8 = (3, 438), (3, 296), (325, 237), (610, 237), \
                                      (910, 320), (910, 438), (590, 290), (340, 290)
     
-    sdc_lanes.vertices = np.array([[p1, p2, p3, p4, p5, p6, p7, p8]], dtype=np.int32)
-    sdc_lanes.region_of_interest()
+    sdc_img.vertices = np.array([[p1, p2, p3, p4, p5, p6, p7, p8]], dtype=np.int32)
+    sdc_img.region_of_interest()
     
     # 6. Use Hough Transform to trace lane lines
-    sdc_lanes.rho = 2
-    sdc_lanes.theta = np.pi/180
-    sdc_lanes.threshold = 100
-    sdc_lanes.min_line_len = 10
-    sdc_lanes.max_line_gap = 30
+    sdc_img.rho = 2
+    sdc_img.theta = np.pi/180
+    sdc_img.threshold = 100
+    sdc_img.min_line_len = 10
+    sdc_img.max_line_gap = 30
     
-    sdc_lanes.hough_function()
+    sdc_img.hough_function()
     
     # 7. Get the inlier of left and right lane lines
-    sdc_lanes.inlier_lines()
+    sdc_img.inlier_lines()
     
     # 8. Draw lines for each lane
-    sdc_lanes.min_lane_size = 240
-    sdc_lanes.max_lane_size = 500
+    sdc_img.min_lane_size = 240
+    sdc_img.max_lane_size = 500
     
-    sdc_lanes.lane_detect()
-    
+    sdc_img.lane_detect()
 
-# Read user input for image path
-parser = argparse.ArgumentParser()
-parser.add_argument("--img_path", help="Image path")
-args = parser.parse_args()
 
-# Dataset path
-ds_path = args.img_path
+def main(dataset_list):
+    for img_name in dataset_list:
+        # Create lane detection object
+        sdc_lanes = Sdclib()
 
-# Image dataset
-dataset = sorted(os.listdir(ds_path))
+        # Get image path
+        sdc_lanes.img_path = ds_path + img_name
 
-for img_name in dataset:
-    # Create lane detection object
-    sdc_lanes = Sdclib()
-    
-    # Get image path
-    sdc_lanes.img_path = ds_path + img_name
+        # Begin execution time
+        begin_time = process_time()
 
-    # Begin execution time
-    begin_time = process_time()
-    
-    # Run pipeline workflow
-    pipeline()
-    
-    # Print the FPS of computed image
-    print(f"Processing image:{sdc_lanes.img_path}",
-          f"\tCPU execution time:{1 / (process_time() - begin_time):0.4f} FPS")
+        # Run pipeline workflow
+        pipeline(sdc_lanes)
+
+        # Print the FPS of computed image
+        fps = process_time() - begin_time
+        if fps <= 0:
+            fps = 0.001
+        print(f"Processing image:{sdc_lanes.img_path}",
+              f"\tCPU execution time:{1 / fps:0.4f} FPS")
+
+
+if __name__ == "__main__":
+    try:
+        # Read user input for image path
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--img_path", help="Image path")
+        args = parser.parse_args()
+
+        # Dataset path
+        ds_path = args.img_path
+
+        # Image dataset
+        dataset = sorted(os.listdir(ds_path))
+
+        main(dataset)
+    except Exception as e:
+        print(e.message, e.args)
