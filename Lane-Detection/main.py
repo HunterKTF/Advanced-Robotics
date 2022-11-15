d253c0643429e35a05d34994164d66ffb796d090
Jorge Ricardo Hernández Sabino <jhernandez@apsjoel.onmicrosoft.com>
2022-11-15 00:37:18 -0600

    Updated functions

--- Lane-Detection/main.py
+++ Lane-Detection/main.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
+# Import standard libraries
+import argparse
+import os
+import numpy as np
+from time import process_time
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Import local libraries
+from sdclibrary import Sdclib
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Pipeline to read
+def pipeline():
+    # 1. Read image with path provided
+    sdc_lanes.read_image()
+    
+    # 2. Convert input image to greyscale
+    sdc_lanes.greyscale()
+    
+    # 3. Apply Gaussian smoothing
+    sdc_lanes.kernel_size = (9, 9)
+    sdc_lanes.gaussian_blur()
+    
+    # 4. Apply Canny Edge detection
+    sdc_lanes.low_threshold = 70
+    sdc_lanes.high_threshold = 100
+    sdc_lanes.canny_edge_det()
+    
+    # 5. Define Region of Interest
+    # Define points for vertices
+    p1, p2, p3, p4, p5, p6, p7, p8 = (3, 438), (3, 296), (325, 237), (610, 237), \
+                                     (910, 320), (910, 438), (590, 290), (340, 290)
+    
+    sdc_lanes.vertices = np.array([[p1, p2, p3, p4, p5, p6, p7, p8]], dtype=np.int32)
+    sdc_lanes.region_of_interest()
+    
+    # 6. Use Hough Transform to trace lane lines
+    sdc_lanes.rho = 2
+    sdc_lanes.theta = np.pi/180
+    sdc_lanes.threshold = 100
+    sdc_lanes.min_line_len = 10
+    sdc_lanes.max_line_gap = 30
+    
+    sdc_lanes.hough_function()
+    
+    # 7. Get the inlier of left and right lane lines
+    sdc_lanes.inlier_lines()
+    
+    # 8. Draw lines for each lane
+    sdc_lanes.min_lane_size = 240
+    sdc_lanes.max_lane_size = 500
+    
+    sdc_lanes.lane_detect()
+    
 
+# Read user input for image path
+parser = argparse.ArgumentParser()
+parser.add_argument("--img_path", help="Image path")
+args = parser.parse_args()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Dataset path
+ds_path = args.img_path
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Image dataset
+dataset = sorted(os.listdir(ds_path))
+
+for img_name in dataset:
+    # Create lane detection object
+    sdc_lanes = Sdclib()
+    
+    # Get image path
+    sdc_lanes.img_path = ds_path + img_name
+
+    # Begin execution time
+    begin_time = process_time()
+    
+    # Run pipeline workflow
+    pipeline()
+    
+    # Print the FPS of computed image
+    print(f"Processing image:{sdc_lanes.img_path}",
+          f"\tCPU execution time:{1 / (process_time() - begin_time):0.4f} FPS")
--- Lane-Detection/sdclibrary.py
+++ Lane-Detection/sdclibrary.py
@@ -1,15 +1,7 @@
-"""sdclibrary.py
-
-This is the main library used to compute a lane detection workflow using edge detection.
-
-Made by: Jorge R. Hernández Sabino
-Last updated: 14 / Nov / 2022
-"""
-
 # Import standard libraries
 import numpy as np
 import cv2
-import math
+from time import sleep
 
 
 class Sdclib():
@@ -22,6 +14,12 @@ class Sdclib():
         self.mask_img = None
         self.img_hough = None
         self.img_inlier = None
+        self.img_lanes = None
+        
+        self.dimensions = ()
+        self.height = 0
+        self.width = 0
+        self.channels = 0
 
         self.kernel_size = ()
         self.low_threshold = 0
@@ -35,23 +33,45 @@ class Sdclib():
         self.max_line_gap = 0
         self.hough_transform = None
 
-        self.GREEN = (255, 0, 0)
+        self.x_left = []
+        self.y_left = []
+        self.x_right = []
+        self.y_right = []
+        
+        self.min_lane_size = 0
+        self.max_lane_size = 0
+
+        self.BLUE = (255, 0, 0)
+        self.GREEN = (0, 255, 0)
+        self.RED = (0, 0, 255)
 
     def read_image(self):
         self.img_color = cv2.imread(self.img_path, cv2.IMREAD_REDUCED_COLOR_4)
-        cv2.imshow("Original Image", self.img_color)
+        
+        # Get image dimensions
+        self.dimensions = self.img_color.shape
+        
+        self.height = self.dimensions[0]
+        self.width = self.dimensions[1]
+        self.channels = self.dimensions[2]
+        
+        # cv2.imshow("Original Image", self.img_color)
+        # cv2.waitKey(1)
 
     def greyscale(self):
         self.grey = cv2.cvtColor(self.img_color, cv2.COLOR_BGR2GRAY)
         # cv2.imshow("Greyscale Image", self.grey)
+        # cv2.waitKey(1)
 
     def gaussian_blur(self):
         self.blur_img = cv2.GaussianBlur(self.grey, self.kernel_size, sigmaX=0, sigmaY=0)
-        # cv2.imshow("Blured Image", self.blur_grey)
+        # cv2.imshow("Blured Image", self.blur_img)
+        # cv2.waitKey(1)
 
     def canny_edge_det(self):
         self.canny = cv2.Canny(self.blur_img, self.low_threshold, self.high_threshold, apertureSize=3)
         # cv2.imshow("Canny Edge Detection", self.canny)
+        # cv2.waitKey(1)
 
     def region_of_interest(self):
         # mask using 0
@@ -59,8 +79,9 @@ class Sdclib():
         cv2.fillPoly(layer, self.vertices, 255)
         self.mask_img = cv2.bitwise_and(self.canny, layer)
         # cv2.imshow("Canny edge detect with RoI", self.mask_img)
+        # cv2.waitKey(1)
 
-    def hough_transform(self):
+    def hough_function(self):
         self.img_hough = self.img_color.copy()
         self.hough_transform = cv2.HoughLinesP(self.mask_img, self.rho, self.theta,
                                           self.threshold, np.array([]),
@@ -71,10 +92,65 @@ class Sdclib():
             for x0, y0, x1, y1 in coords:
                 cv2.line(self.img_hough, (x0, y0), (x1, y1), self.GREEN, 5)
         # cv2.imshow('Hough lines', self.img_hough)
+        # cv2.waitKey(1)
 
     def inlier_lines(self):
         self.img_inlier = self.img_color.copy()
 
+        for coords in self.hough_transform:
+            for x0, y0, x1, y1 in coords:
+                mx_slope = (y1 - y0) / (x1 - x0)
+                if abs(mx_slope) > 0.28:
+                    
+                    if mx_slope <= 0:
+                        if x0 <= (self.width/2) and x1 <= (self.width/2):
+                            self.x_left.extend((x0, x1))
+                            self.y_left.extend((y0, y1))
+                            cv2.line(self.img_inlier, (x0, y0), (x1, y1), self.BLUE, 5)
+                    
+                    else:
+                        if x0 > (self.width/2) and x1 > (self.width/2):
+                            self.x_right.extend((x0, x1))
+                            self.y_right.extend((y0, y1))
+                            cv2.line(self.img_inlier, (x0, y0), (x1, y1), self.RED, 5)
+                            
+        # cv2.imshow('Lane lines', self.img_inlier)
+        # cv2.waitKey(1)
+        
+    def lane_detect(self):
+        self.img_lanes = self.img_color.copy()
 
+        if self.x_left and self.y_left and self.x_right and self.y_right:
 
-print("ok")
+            left_line = np.poly1d(np.polyfit(self.y_left, self.x_left, deg=1))
+            right_line = np.poly1d(np.polyfit(self.y_right, self.x_right, deg=1))
+            
+            right_init = int(right_line(self.max_lane_size))
+            right_end = int(right_line(self.min_lane_size))
+            
+            left_init = int(left_line(self.max_lane_size))
+            left_end = int(left_line(self.min_lane_size))
+            
+            lines = [[
+                [left_init, self.max_lane_size, left_end, self.min_lane_size],
+                [right_init, self.max_lane_size, right_end, self.min_lane_size]
+            ]]
+            pts = []
+            for line in lines:
+                for x0, y0, x1, y1 in line:
+                    cv2.line(self.img_color, (x0, y0), (x1, y1), self.RED, 6)
+
+                    if x0 <= (self.width / 2) and x1 <= (self.width / 2):
+                        pts.append([x0, y0])
+                        pts.append([x1, y1])
+                    else:
+                        pts.append([x1, y1])
+                        pts.append([x0, y0])
+                        
+            points = np.array(pts, np.int32)
+            cv2.fillPoly(self.img_lanes, pts=[points], color=self.GREEN)
+            alpha = 0.4
+            img_out = cv2.addWeighted(self.img_lanes, alpha, self.img_color, 1-alpha, 0)
+                    
+        cv2.imshow('Lane detection', img_out)
+        cv2.waitKey(1)
